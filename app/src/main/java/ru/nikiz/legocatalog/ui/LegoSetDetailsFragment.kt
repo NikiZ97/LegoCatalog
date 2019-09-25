809e18ff74551c3acd68e42edfbe882e46f2e019
NikiZ97 <sharonov.nik1997@gmail.com>
2019-09-25 12:01:15 +0300

    Navigation between fragments added

---
+++ app/src/main/java/ru/nikiz/legocatalog/ui/LegoSetDetailsFragment.kt
@@ -0,0 +1,25 @@
+package ru.nikiz.legocatalog.ui
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.nikiz.legocatalog.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LegoSetDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lego_set_details, container, false)
+    }
+
+
+}
