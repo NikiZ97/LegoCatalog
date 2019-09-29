9c08cdbbbce0341f0035037b3ffa1d12847401f2
NikiZ97 <sharonov.nik1997@gmail.com>
2019-09-29 11:45:16 +0300

    Customizing theme list

--- app/src/main/java/ru/nikiz/legocatalog/ui/themes/LegoThemeFragment.kt
+++ app/src/main/java/ru/nikiz/legocatalog/ui/themes/LegoThemeFragment.kt
@@ -8,13 +8,13 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_lego_theme.*
 import org.koin.android.viewmodel.ext.android.viewModel
 import ru.nikiz.data.extensions.toggleVisibility
 import ru.nikiz.domain.Result
 import ru.nikiz.legocatalog.R
+import ru.nikiz.legocatalog.ui.util.LegoThemeItemDecoration
 
 class LegoThemeFragment : Fragment() {
 
@@ -30,7 +30,9 @@ class LegoThemeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         themeAdapter = LegoThemesAdapter()
         legoThemes.layoutManager = layoutManager
-        legoThemes.addItemDecoration(DividerItemDecoration(context, layoutManager.orientation))
+        legoThemes.addItemDecoration(LegoThemeItemDecoration(
+            resources.getDimension(R.dimen.lego_theme_list_item_space).toInt()
+        ))
         legoThemes.adapter = themeAdapter
         subscribeUi()
     }
---
+++ app/src/main/java/ru/nikiz/legocatalog/ui/util/LegoThemeItemDecoration.kt
@@ -0,0 +1,16 @@
+package ru.nikiz.legocatalog.ui.util
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+
+class LegoThemeItemDecoration(private val verticalSpaceHeight: Int): RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+        if (parent.getChildAdapterPosition(view) != parent.adapter!!.itemCount - 1) {
+            outRect.bottom = verticalSpaceHeight
+        }
+    }
+}
